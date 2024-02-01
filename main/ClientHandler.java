39a1d2e7a66145c14c527b3ec5a0bc3fe0dc798e
dewnithathsara <139765543+dewnithathsara@users.noreply.github.com>
2024-02-01 22:38:33 +0530

    Create ClientHandler.java

---
+++ main/ClientHandler.java
@@ -0,0 +1,58 @@
+package com.example.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+
+
+
+        private ArrayList<ClientHandler> clients;
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        public ClientHandler(Socket socket, ArrayList<ClientHandler> clients) {
+            try {
+                this.socket = socket;
+                this.clients = clients;
+                this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.writer = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                String msg;
+                while ((msg = reader.readLine()) != null) {
+                    if (msg.equalsIgnoreCase( "exit")) {
+                        break;
+                    }
+                    for (ClientHandler clientHandler : clients) {
+                        clientHandler.writer.println(msg);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    reader.close();
+                    writer.close();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+
+}
